f9d0714b933e0562a38ee7cda22c9becb68fe824
Sabyrka <zensabyrka@gmail.com>
2026-04-20 00:02:27 +0500

    feat: restructure todo components and implement AddTaskForm, SearchTaskForm, TodoInfo, TodoList, and TodoItem

---
+++ src/components/AddTaskForm.jsx
@@ -0,0 +1,13 @@
+import Field from './Field'
+import Button from './Button'
+
+const AddTaskForm = () => {
+  return (
+    <form className="todo__form">
+      <Field />
+      <Button />
+    </form>
+  )
+}
+
+export default AddTaskForm
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+const Button = () => {
+  return (
+    <button
+      className="button"
+      type="submit"
+    >
+      Add
+    </button>
+  )
+}
+
+export default Button
